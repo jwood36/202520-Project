e947134d7f3f7c3024604d1b366e7e7215d6c0f1
jwood36 <53241126+jwood36@users.noreply.github.com>
2025-02-17 19:33:18 -0600

    Added GetQuestion(GUID) function

--- src/data/TestDB.cs
+++ src/data/TestDB.cs
@@ -23,6 +23,142 @@ namespace coding_lms.data {
 			// Return Value Result
 			return ret;
 		}
+#if DEBUG
+		// This is to test functionality during ALPHA DEBUG development
+		public IEnumerable<Question> GetQuestion(Guid pooltest) {
+
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.TF,
+				Body = "This is Question #1 Body",
+				IsRandom = true,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){ Text = "Answer 1.1", IsCorrect=true, Ordinal=1}
+					, new Answer(Guid.NewGuid()){Text = "Answer 1.2", IsCorrect=false, Ordinal=2}
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MAL,
+				Body = "This is Question #2 Body",
+				IsRandom = true,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="A", Text="Answer 2.1", IsCorrect=false, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="B", Text="Answer 2.2", IsCorrect=true, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="C", Text="Answer 2.3", IsCorrect=true, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="D", Text="Answer 2.4", IsCorrect=false, Ordinal=4 },
+					new Answer(Guid.NewGuid()){Key="E", Text="Answer 2.5", IsCorrect=true, Ordinal=5 },
+					new Answer(Guid.NewGuid()){Key="F", Text="Answer 2.6", IsCorrect=false, Ordinal=6 },
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MAN,
+				Body = "This is Question #3 Body",
+				IsRandom = true,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="1", Text="Answer 3.1", IsCorrect=false, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="2", Text="Answer 3.2", IsCorrect=false, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="3", Text="Answer 3.3", IsCorrect=true, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="4", Text="Answer 3.4", IsCorrect=true, Ordinal=4 },
+					new Answer(Guid.NewGuid()){Key="5", Text="Answer 3.5", IsCorrect=true, Ordinal=5 },
+					new Answer(Guid.NewGuid()){Key="6", Text="Answer 3.6", IsCorrect=false, Ordinal=6 },
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MCN,
+				Body = "This is Question #4 Body",
+				IsRandom = true,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="1", Text="Answer 4.1", IsCorrect=false, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="2", Text="Answer 4.2", IsCorrect=false, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="3", Text="Answer 4.3", IsCorrect=true, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="4", Text="Answer 4.4", IsCorrect=false, Ordinal=4 },
+					new Answer(Guid.NewGuid()){Key="5", Text="Answer 4.5", IsCorrect=false, Ordinal=5 },
+					new Answer(Guid.NewGuid()){Key="6", Text="Answer 4.6", IsCorrect=false, Ordinal=6 },
+					new Answer(Guid.NewGuid()){Key="7", Text="Answer 4.7", IsCorrect=false, Ordinal=7 },
+					new Answer(Guid.NewGuid()){Key="8", Text="Answer 4.8", IsCorrect=false, Ordinal=8 },
+					new Answer(Guid.NewGuid()){Key="9", Text="Answer 4.9", IsCorrect=false, Ordinal=9 },
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MCL,
+				Body = "This is Question #5 Body",
+				IsRandom = true,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="A", Text="Answer 5.1", IsCorrect=false, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="B", Text="Answer 5.2", IsCorrect=true, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="C", Text="Answer 5.3", IsCorrect=false, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="D", Text="Answer 5.4", IsCorrect=false, Ordinal=4 },
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.TF,
+				Body = "This is Question #6 Body",
+				IsRandom = false,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){ Key="1", Text = "Answer 6.1", IsCorrect=true, Ordinal=4}
+					, new Answer(Guid.NewGuid()){Key="2", Text = "Answer 6.2", IsCorrect=false, Ordinal=9}
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MCL,
+				Body = "This is Question #7 Body",
+				IsRandom = false,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="A", Text="Answer 7.1", IsCorrect=false, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="B", Text="Answer 7.2", IsCorrect=true, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="C", Text="Answer 7.3", IsCorrect=false, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="D", Text="Answer 7.4", IsCorrect=false, Ordinal=4 },
+					new Answer(Guid.NewGuid()){Key="E", Text="Answer 7.4", IsCorrect=false, Ordinal=5 },
+					new Answer(Guid.NewGuid()){Key="F", Text="Answer 7.4", IsCorrect=false, Ordinal=6 },
+					new Answer(Guid.NewGuid()){Key="G", Text="Answer 7.4", IsCorrect=false, Ordinal=7 },
+					new Answer(Guid.NewGuid()){Key="H", Text="Answer 7.4", IsCorrect=false, Ordinal=8 },
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MCN,
+				Body = "This is Question #8 Body",
+				IsRandom = false,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="1", Text="Answer 8.1", IsCorrect=false, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="2", Text="Answer 8.2", IsCorrect=false, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="3", Text="Answer 8.3", IsCorrect=true, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="4", Text="Answer 8.4", IsCorrect=false, Ordinal=4 },
+					new Answer(Guid.NewGuid()){Key="5", Text="Answer 8.5", IsCorrect=false, Ordinal=5 },
+					new Answer(Guid.NewGuid()){Key="6", Text="Answer 8.6", IsCorrect=false, Ordinal=6 },
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MAN,
+				Body = "This is Question #9 Body",
+				IsRandom = false,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="1", Text="Answer 9.1", IsCorrect=false, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="2", Text="Answer 9.2", IsCorrect=false, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="3", Text="Answer 9.3", IsCorrect=true, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="4", Text="Answer 9.4", IsCorrect=true, Ordinal=4 },
+					new Answer(Guid.NewGuid()){Key="5", Text="Answer 9.5", IsCorrect=true, Ordinal=5 },
+					new Answer(Guid.NewGuid()){Key="6", Text="Answer 9.6", IsCorrect=false, Ordinal=6 },
+				}
+			};
+			yield return new Question(Guid.NewGuid()) {
+				Type = QuestionEnum.MAL,
+				Body = "This is Question #10 Body",
+				IsRandom = false,
+				Answers = new List<Answer>() {
+					new Answer(Guid.NewGuid()){Key="A", Text="Answer 10.1", IsCorrect=true, Ordinal=1 },
+					new Answer(Guid.NewGuid()){Key="B", Text="Answer 10.2", IsCorrect=true, Ordinal=2 },
+					new Answer(Guid.NewGuid()){Key="C", Text="Answer 10.3", IsCorrect=false, Ordinal=3 },
+					new Answer(Guid.NewGuid()){Key="D", Text="Answer 10.4", IsCorrect=false, Ordinal=4 },
+					new Answer(Guid.NewGuid()){Key="E", Text="Answer 10.5", IsCorrect=false, Ordinal=5 },
+					new Answer(Guid.NewGuid()){Key="F", Text="Answer 10.6", IsCorrect=false, Ordinal=6 },
+				}
+			};
+#else
+		public Question GetQuestion(Guid pooltest) {
+			Question ret;
+
+						return ret;
+#endif
+		}
 		#region Attempt ONLY Method/Functions
 		/// <summary>
 		/// Returns a single Attempt record
