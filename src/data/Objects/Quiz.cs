3b7d3541aa6e2da3a637bca34a4deccb52379ab9
jwood36 <53241126+jwood36@users.noreply.github.com>
2025-02-17 19:22:39 -0600

    Added Constructor chain to Quiz class

--- src/data/Objects/Quiz.cs
+++ src/data/Objects/Quiz.cs
@@ -1,4 +1,37 @@
-﻿namespace coding_lms.data {
+﻿using System;
+
+namespace coding_lms.data {
 	public class Quiz {
+		public Quiz(int term, string crn, string name) : this() {
+			this._term = term;
+			this._crn = crn;
+			this._name = name;
+		}
+		private Quiz() {
+			this.Init();
+		}
+
+		private void Init() {
+			return;
+		}
+
+		private int _term;
+		public int Term {
+			get { return _term; }
+			set { this._term = value; }
+		}
+
+		private string _crn;
+		public string Crn {
+			get { return _crn; }
+			set { this._crn = value; }
+		}
+
+		private string _name;
+		public string Name {
+			get { return _name; }
+			set { this._name = value; }
+		}
+
 	}
 }
