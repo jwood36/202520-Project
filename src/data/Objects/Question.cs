2d331db99d58f751e2129a25756ea2d3b6738509
jwood36 <53241126+jwood36@users.noreply.github.com>
2025-02-17 19:28:07 -0600

    - Added Answers property to Question class
    - Added Constructors to Question class

--- src/data/Objects/Question.cs
+++ src/data/Objects/Question.cs
@@ -1,4 +1,13 @@
 ﻿namespace coding_lms.data {
 	public class Question {
+		public Question(Guid id) : this() { }
+		public Question(long id) : this() { }
+		private Question() {
+			this._answ = new List<Answer>();
+		}
+
+		private IEnumerable<Answer> _answ;
+		public IEnumerable<Answer> Answers { get; set; }
+
 	}
 }
