53b177be16e4c62673f0c35fb8f4840b85f0a412
jwood36 <53241126+jwood36@users.noreply.github.com>
2025-03-05 21:54:27 -0600

    Added Term-oriented method and Object to the InstructorDB object

--- src/data/InstructorDB.cs
+++ src/data/InstructorDB.cs
@@ -230,6 +230,23 @@ namespace coding_lms.data {
 			return null;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public IEnumerable<Term> GetTerms() {
+#if DEBUG
+			return new List<Term>(){
+				new Term{ID = 202520, SchoolYear=2025, Name=TermEnum.Spring},
+				new Term{ID = 202510, SchoolYear=2025, Name=TermEnum.Fall },
+				new Term{ID = 202430, SchoolYear=2024, Name=TermEnum.Summer },
+				new Term{ID=202420, SchoolYear=2024, Name=TermEnum.Spring}
+			};
+#else
+			return null;
+#endif
+		}
+
 
 	}
 }
