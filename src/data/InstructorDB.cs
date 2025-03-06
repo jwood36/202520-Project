f90a95fd3562e2da1a95cfdf9ee7d03f33ced8d1
jwood36 <53241126+jwood36@users.noreply.github.com>
2025-03-05 21:51:34 -0600

    Refactored the Quiz-oriented methods on the InstructorDB object

--- src/data/InstructorDB.cs
+++ src/data/InstructorDB.cs
@@ -27,12 +27,33 @@ namespace coding_lms.data {
 				).Single();
 		}
 
+#if DEBUG
+		private IEnumerable<Quiz> _quizzes = new List<Quiz>(){
+				new Quiz{ ID = 1, UUID=Guid.NewGuid(), Name = "Quiz #1", Short = "Quiz_1", IsRandom = true, PPQ = 10, Time = 1024 }
+				, new Quiz{ID = 3, UUID=Guid.NewGuid(), Name = "Quiz #2", Short = "Quiz_2", IsRandom = false, PPQ = 1, Time = 2048 }
+				, new Quiz{ID = 5, UUID=Guid.NewGuid(), Name = "Quiz #3", Short = "Quiz_3", IsRandom = true, PPQ = 100, Time = 512 }
+				, new Quiz{ID = 7, UUID=Guid.NewGuid(), Name = "Quiz #4", Short = "Quiz_4", IsRandom = false, PPQ = 5, Time = 4096 }
+				, new Quiz{ID = 9, UUID=Guid.NewGuid(), Name = "Quiz #5", Short = "Quiz_5", IsRandom = false, PPQ = 5000, Time = 256 }
+			};
+#endif
 		/// <summary>
 		/// Returns all Quizzes currently available
 		/// </summary>
 		/// <returns></returns>
-		public IEnumerable<Quiz> GetQuizzes() {
+		public IEnumerable<QuizView> GetQuizzesView() {
+#if DEBUG
+			return this._quizzes;
+#else
 			return null;
+#endif
+		}
+
+		public Quiz GetQuizDetails(long id) {
+#if DEBUG
+			return this._quizzes.FirstOrDefault(x => x.ID == id);
+#else
+			return null;
+#endif
 		}
 
 		/// <summary>
