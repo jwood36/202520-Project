452e6c0a331041a311798d4b23b0177f8a528d55
Petercorbschool <peter.corbitt@jeffersonstate.edu>
2025-03-11 20:37:50 -0500

    corrected to content placeholder

--- src/quiz/Site.Master.designer.cs
+++ src/quiz/Site.Master.designer.cs
@@ -21,7 +21,7 @@ namespace quiz
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder HeadContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
 
         /// <summary>
         /// cphNavBar control.
